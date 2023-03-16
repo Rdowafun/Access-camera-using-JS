7457778775a5a724b27caecb0182360e025f7e82
Rdowafun <jabrooster@gmail.com>
2023-03-16 13:10:12 +0500

    change event

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-const btn = document.querySelector('button')
-
-
-btn.addEventListener('click', () => {
+window.addEventListener('load', () => {
     const video = document.querySelector('video')
     const options = {
         video: true
